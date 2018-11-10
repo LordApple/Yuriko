df762a35fb07fce1efd211d23ad6667238679434
Apple <lordapplz@protonmail.com>
2018-11-10 23:07:13 +0100

    update
    added reload and unload command
    fixed invite link message

--- extensions/core.py
+++ extensions/core.py
@@ -8,7 +8,7 @@ class Core:
 
     @commands.command()
     async def invite(self, ctx):
-        await ctx.send("Invite link:https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8".format(self.bot.user.id))
+        await ctx.send("Invite link: https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8".format(self.bot.user.id))
 
     @commands.command()
     @commands.is_owner()
@@ -23,6 +23,25 @@ class Core:
             stack_line = str(e).split('\n')[-1]
             await m.edit(content=f'Error while loading {name}\n`{stack_line}`')
 
+    @commands.command()
+    async def unload(self, ctx, name: str):
+        """Unloads an extension in the bot"""
+        m = await ctx.send(f'unloading {name}')
+        try:
+            self.bot.unload_extension(f'extensions.{name}')
+            await m.edit(content='Extension unloaded.')
+        except:
+            return
+
+    @commands.command()
+    async def load(self, ctx, name: str):
+        """Loads an extension in the bot"""
+        m = await ctx.send(f"loading {name}")
+        try:
+            self.bot.load_extension(f'extensions.{name}')
+            await m.edit(content="Extension loaded.")
+        except:
+            return
 
 def setup(bot):
     bot.add_cog(Core(bot))
