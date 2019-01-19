d58921fe93725f9472ae1dcf5a68da7fcf6f97d7
Unknown <lordapplz@protonmail.com>
2019-01-20 00:16:21 +0100

    Added emojify command

--- extensions/misc.py
+++ extensions/misc.py
@@ -450,5 +450,25 @@ class Misc:
                     embed.set_image(url=(js['url']))
                     await ctx.send(embed=embed)
 
+    @commands.command()
+    async def emojify(self,ctx,*,args):
+        emojis = {"a": ":regional_indicator_a:","b": ":regional_indicator_b:","c":":regional_indicator_c:","d":":regional_indicator_d:","e":":regional_indicator_e:","f":":regional_indicator_f:"
+        ,"g":":regional_indicator_g:","h":":regional_indicator_h:","i":":regional_indicator_i:","j":":regional_indicator_j:","k":":regional_indicator_k:","l":":regional_indicator_l:"
+        ,"m":":regional_indicator_m:","n":":regional_indicator_n:","o":":regional_indicator_o:","p":":regional_indicator_p:","q":":regional_indicator_q:","r":":regional_indicator_r:"
+        ,"s":":regional_indicator_s:","t":":regional_indicator_t:","u":":regional_indicator_u:","v":":regional_indicator_v:","w":":regional_indicator_w:","x":":regional_indicator_x:"
+        ,"y":":regional_indicator_y:","z":":regional_indicator_z:"," ": "  "
+        }
+        msg = args.lower()
+        new_msg = ""
+        for x in msg:
+            if x in emojis:
+                new_msg += f"{emojis[x]}"
+        if len(new_msg) >= 2000:
+            for page in pagify(new_msg,escape=False):
+                await ctx.send(page)
+            await ctx.send("Message is over 2000 chars, so i split the msg")
+        else:
+            await ctx.send(new_msg)
+
 def setup(bot):
     bot.add_cog(Misc(bot))
